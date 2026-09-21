3bcb878e4b311659800358aff2e78786b1460f68
XKuijpers <144124949+XKuijpers@users.noreply.github.com>
2026-09-21 14:43:26 +0200

    filled gallery

--- barber_webshop/frontend/src/components/Gallery.vue
+++ barber_webshop/frontend/src/components/Gallery.vue
@@ -1,19 +1,17 @@
 <template>
   <div id="gallerySection" class="pt-16 md:pt-30 lg:pt-55">
     <div class="grid grid-cols-12 gap-2 md:gap-6">
-      <div
-        class="accent border-highlight col-span-full h-[137px] border-4 md:col-span-8 md:h-[300px]"
-      ></div>
-      <div
-        class="accent border-highlight col-span-6 h-[137px] border-4 md:col-span-4 md:h-[300px]"
-      ></div>
+      <img src="../assets/gallery2.png" alt=""
+           class="accent border-highlight col-span-6 border-4 md:col-span-4 h-full"
+      >
 
-      <div
-        class="accent border-highlight col-span-6 h-[137px] border-4 md:col-span-4 md:h-[300px]"
-      ></div>
-      <div
-        class="accent border-highlight col-span-full h-[137px] border-4 md:col-span-8 md:h-[300px]"
-      ></div>
+      <img src="../assets/gallery3.png" alt=""
+           class="accent border-highlight col-span-6 border-4 md:col-span-4 h-full"
+      >
+
+      <img src="../assets/gallery4.png" alt=""
+           class="accent border-highlight col-span-full border-4 md:col-span-4 h-full"
+      >
     </div>
   </div>
 </template>
