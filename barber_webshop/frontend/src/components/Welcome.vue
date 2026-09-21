b281a886ded4a3566085b4372ac7fa6cf8121f35
XKuijpers <144124949+XKuijpers@users.noreply.github.com>
2026-09-21 14:06:15 +0200

    added site logo

--- barber_webshop/frontend/src/components/Welcome.vue
+++ barber_webshop/frontend/src/components/Welcome.vue
@@ -6,9 +6,10 @@
       Barbershop Slogan Lorem Ipsum Sit Doler
     </p>
 
-    <div
-      class="accent mx-auto mt-8 h-[305px] w-[378px] md:mt-4 md:h-[400px] md:w-[450px] lg:mt-16"
-    ></div>
+    <img src="../assets/barber_webshop.png" alt=""
+         class="mx-auto mt-8 h-[305px] w-fit md:mt-4 md:h-[400px] lg:mt-16"
+    >
   </div>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+</script>
